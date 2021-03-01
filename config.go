ff50fd3d59f91000455a1dc5bffe2739d18e4d69
Andrey Semochkin <admin@deepweb.spb.ru>
2021-03-01 15:57:42 +0300

    test bad sps pps

--- config.go
+++ config.go
@@ -9,6 +9,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/deepch/vdk/codec/h264parser"
+
 	"github.com/deepch/vdk/av"
 )
 
@@ -147,6 +149,21 @@ func (element *ConfigST) coGe(suuid string) []av.CodecData {
 			return nil
 		}
 		if tmp.Codecs != nil {
+			//TODO Delete test
+			for _, codec := range tmp.Codecs {
+				if codec.Type() == av.H264 {
+					codecVideo := codec.(h264parser.CodecData)
+					if codecVideo.SPS() != nil && codecVideo.PPS() != nil && len(codecVideo.SPS()) > 0 && len(codecVideo.PPS()) > 0 {
+						//ok
+						//log.Println("Ok Video Ready to play")
+					} else {
+						//video codec not ok
+						log.Println("Bad Video Codec SPS or PPS Wait")
+						time.Sleep(50 * time.Millisecond)
+						continue
+					}
+				}
+			}
 			return tmp.Codecs
 		}
 		time.Sleep(50 * time.Millisecond)
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/deepch/RTSPtoWebRTC
 go 1.15
 
 require (
-	github.com/deepch/vdk v0.0.0-20210215224601-dd938d5e1108
+	github.com/deepch/vdk v0.0.0-20210218105644-f6de3acad035
 	github.com/gin-gonic/gin v1.6.3
 )
