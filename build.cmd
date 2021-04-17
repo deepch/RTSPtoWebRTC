8543f6d064b3cc96b10fa15706b61ca7c18eb179
Alexey Khit <alexey.khit@gmail.com>
2021-04-17 06:43:15 +0300

    Add darwin and i386 archs

--- build.cmd
+++ build.cmd
@@ -2,17 +2,25 @@
 
 @SET GOOS=windows
 @SET GOARCH=amd64
-go build -ldflags "-s -w" -o rtsp2webrtc_amd64.exe
+go build -ldflags "-s -w" -o bin/rtsp2webrtc_amd64.exe
+
+@SET GOOS=linux
+@SET GOARCH=386
+go build -ldflags "-s -w" -o bin/rtsp2webrtc_i386
 
 @SET GOOS=linux
 @SET GOARCH=amd64
-go build -ldflags "-s -w" -o rtsp2webrtc_amd64
+go build -ldflags "-s -w" -o bin/rtsp2webrtc_amd64
 
 @SET GOOS=linux
 @SET GOARCH=arm
 @SET GOARM=7
-go build -ldflags "-s -w" -o rtsp2webrtc_armv7
+go build -ldflags "-s -w" -o bin/rtsp2webrtc_armv7
 
 @SET GOOS=linux
 @SET GOARCH=arm64
-go build -ldflags "-s -w" -o rtsp2webrtc_aarch64
+go build -ldflags "-s -w" -o bin/rtsp2webrtc_aarch64
+
+@SET GOOS=darwin
+@SET GOARCH=arm64
+go build -ldflags "-s -w" -o bin/rtsp2webrtc_darwin
